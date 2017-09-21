c9086230c5999b376f87fa98075554efb9da057c
Ferhat YEŞİLYURT <fyeilyurt@gmail.com>
2017-09-22 01:05:52 +0300

    Sefer Bilgileri Eklendi.

--- OtobusOtomasyonu/OtobusOtomasyonu/Form1.Designer.cs
+++ OtobusOtomasyonu/OtobusOtomasyonu/Form1.Designer.cs
@@ -29,15 +29,56 @@
         private void InitializeComponent()
         {
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.seferlerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sakaryaİstanbulToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(15, 12);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(15, 27);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(200, 459);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(200, 444);
             this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.seferlerToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(248, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // seferlerToolStripMenuItem
+            // 
+            this.seferlerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sakaryaİstanbulToolStripMenuItem});
+            this.seferlerToolStripMenuItem.Name = "seferlerToolStripMenuItem";
+            this.seferlerToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.seferlerToolStripMenuItem.Text = "Seferler";
+            // 
+            // sakaryaİstanbulToolStripMenuItem
+            // 
+            this.sakaryaİstanbulToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBox1});
+            this.sakaryaİstanbulToolStripMenuItem.Name = "sakaryaİstanbulToolStripMenuItem";
+            this.sakaryaİstanbulToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.sakaryaİstanbulToolStripMenuItem.Text = "Sakarya-İstanbul";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Items.AddRange(new object[] {
+            "07:00",
+            "09:00",
+            "13:00",
+            "20:00"});
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 23);
             // 
             // Form1
             // 
@@ -45,16 +86,25 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(248, 424);
             this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem seferlerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sakaryaİstanbulToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
     }
 }
 
