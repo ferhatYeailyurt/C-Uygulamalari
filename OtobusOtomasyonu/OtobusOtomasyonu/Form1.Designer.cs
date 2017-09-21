84eaa1a422aaf361795bbf4c2a797f6dcd3e258f
Ferhat YEŞİLYURT <fyeilyurt@gmail.com>
2017-09-22 01:15:12 +0300

    Sefer bilgisine göre araç koltuk sayısını değiştirme işlemi yapıldı.

--- OtobusOtomasyonu/OtobusOtomasyonu/Form1.Designer.cs
+++ OtobusOtomasyonu/OtobusOtomasyonu/Form1.Designer.cs
@@ -79,6 +79,7 @@
             "20:00"});
             this.toolStripComboBox1.Name = "toolStripComboBox1";
             this.toolStripComboBox1.Size = new System.Drawing.Size(121, 23);
+            this.toolStripComboBox1.SelectedIndexChanged += new System.EventHandler(this.toolStripComboBox1_SelectedIndexChanged);
             // 
             // Form1
             // 
--- OtobusOtomasyonu/OtobusOtomasyonu/Form1.cs
+++ OtobusOtomasyonu/OtobusOtomasyonu/Form1.cs
@@ -19,11 +19,15 @@ namespace OtobusOtomasyonu
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //koltuk adeti ile orta kapının yerini paremetre olarak gönderdik
             KoltukDoldur(60, 7);
         }
 
         void KoltukDoldur(int koltukSayisi,int sira)
         {
+            //formu temizleyip yeniden koltukları sıralamak için.
+            flowLayoutPanel1.Controls.Clear();
+
             for (int i = 1; i <= koltukSayisi; i++)
             {
                 Button koltuk = new Button();
@@ -47,6 +51,25 @@ namespace OtobusOtomasyonu
                 flowLayoutPanel1.Controls.Add(koltuk); 
             }
 
+        }
+
+        private void toolStripComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch(toolStripComboBox1.SelectedItem.ToString())
+            {//sefer saatine göre araç koltuk sayısını ve kapı yerine belirler
+                case "07:00":
+                    KoltukDoldur(46, 7);
+                    break;
+                case "09:00":
+                     KoltukDoldur(42, 6);
+                    break;
+                case "13:00":
+                    KoltukDoldur(50, 8);
+                    break;
+                case "20:00":
+                    KoltukDoldur(54, 8);
+                    break;
+            }
         }
     }
 }
